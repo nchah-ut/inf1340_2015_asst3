bbb0af49fae97e80c66444f1a1aded34747285f7
Niel (iSchool) <niel.chah@mail.utoronto.ca>
2015-12-16 18:52:37 -0500

    PEP8 fixes

--- test_exercise2.py
+++ test_exercise2.py
@@ -31,35 +31,35 @@ def test_returning():
         ["Accept", "Accept", "Quarantine"]
 
 def test_correct_date_format():
-	"""
-	Test to see if different dates are accepted.
-	"""
-	try:
-		assert valid_date_format("2015-02-22") 
-	except True:
-		return True
+    """
+    Test to see if different dates are accepted.
+    """
+    try:
+        assert valid_date_format("2015-02-22")
+    except True:
+        return True
 
-	try:	
-		assert valid_date_format("2012-30-40")
-	except False:
-		return True
+    try:
+        assert valid_date_format("2012-30-40")
+    except False:
+        return True
 
-	try:
-		assert valid_date_format(2015-02-22)
-	except TypeError:
-		return True	
+    try:
+        assert valid_date_format(2015-02-22)
+    except TypeError:
+        return True
 
-def test_correct__visa_format():
-	"""
-	Test to see if the visa is accepted
-	"""
-	
-	try:
-		assert correct__visa_format("CFR6X-XSMVA")
-	except True:
-		return True
+def test_correct_visa_format():
+    """
+    Test to see if the visa is accepted
+    """
 
-	try:
-		assert correct__visa_format(99999-9999)
-	except TypeError:
-		return True
+    try:
+        assert valid_visa_format("CFR6X-XSMVA")
+    except True:
+        return True
+
+    try:
+        assert valid_visa_format(99999-9999)
+    except TypeError:
+        return True
